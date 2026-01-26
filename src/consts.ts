4690bddd2cf1b269a9313e4dbb8db18380e658ad
z6nc <andresvelasquezarotinco@gmail.com>
2026-01-25 19:51:48 -0500

    update favicon and site constants for ARX STAR Blog

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'ARX STAR Blog';
+export const SITE_DESCRIPTION = 'Bienvenidos a ARX STAR Blog, tu fuente confiable de noticias y análisis sobre tecnología, ciencia y cultura digital. Explora artículos profundos, reseñas de productos y las últimas tendencias en el mundo tecnológico.';
